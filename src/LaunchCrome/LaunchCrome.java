dc5cc71c23fa200a9d216e285344a457741379d8
bari523 <bari523@yahoo.com>
2020-05-09 14:06:47 -0500

    first commit

---
+++ src/LaunchCrome/LaunchCrome.java
@@ -0,0 +1,14 @@
+package LaunchCrome;
+
+import org.openqa.selenium.webDriver;
+import org.openqa.selenium.ChromeDriver;
+
+public class LaunchCrome {
+public static void main (String[] args) {
+	
+	System.setProperty("webdriver.chrome.driver", "")
+	
+	
+}
+
+}
